5c6c607678d645686a80e832eae8aa8fa9da6c4e
Henrique Resende <Henrique3605@hotmail.com>
2024-10-15 17:36:17 -0300

    Ajuste para usuários novos começarem com a imagem selecionada

--- GymHerosDB/dbo/Stored Procedures/InsertUser.sql
+++ GymHerosDB/dbo/Stored Procedures/InsertUser.sql
@@ -1,4 +1,4 @@
-﻿CREATE   PROCEDURE InsertUser
+﻿CREATE PROCEDURE [dbo].[InsertUser]
 @Name VARCHAR(250),
 @Login VARCHAR(250),
 @Password VARCHAR(250),
@@ -13,6 +13,7 @@ BEGIN
 		,[Password]
 		,[Weight]
 		,[Height]
+		,[Image]
 		,[Vitality]
 		,[Agility]
 		,[Defense]
@@ -25,6 +26,7 @@ BEGIN
         ,@Password
         ,@Weight
         ,@Height
+		,'0'
 		,5
 		,5
 		,5
@@ -35,39 +37,47 @@ BEGIN
 	SELECT @IdUser = SCOPE_IDENTITY()
 
 	INSERT INTO [dbo].[Mission]
-           ([IdUser], [Type], [TargetType], [Target], [Reward], [Done])
+        ([IdUser], [Type], [TargetType], [Target], [Reward], [Done])
      VALUES
-           (@IdUser, 'Mission', 'WorkoutCount', 5, 10, 0),
-           (@IdUser, 'Mission', 'WorkoutTime', 4, 10, 0),
-           (@IdUser, 'Mission', 'VolumeWorkout', 100, 10, 0),
-           (@IdUser, 'Mission', 'VolumeTotal', 700, 10, 0),
+        (@IdUser, 'Mission', 'WorkoutCount', 5, 10, 0),
+        (@IdUser, 'Mission', 'WorkoutTime', 4, 10, 0),
+        (@IdUser, 'Mission', 'VolumeWorkout', 100, 10, 0),
+        (@IdUser, 'Mission', 'VolumeTotal', 700, 10, 0),
 
-           (@IdUser, 'Achievement', 'WorkoutCount', 1, 10, 0),
-           (@IdUser, 'Achievement', 'WorkoutCount', 10, 10, 0),
-           (@IdUser, 'Achievement', 'WorkoutCount', 50, 30, 0),
-           (@IdUser, 'Achievement', 'WorkoutCount', 75, 50, 0),
-           (@IdUser, 'Achievement', 'WorkoutCount', 100, 100, 0),
+        (@IdUser, 'Achievement', 'WorkoutCount', 1, 10, 0),
+        (@IdUser, 'Achievement', 'WorkoutCount', 10, 10, 0),
+        (@IdUser, 'Achievement', 'WorkoutCount', 50, 30, 0),
+        (@IdUser, 'Achievement', 'WorkoutCount', 75, 50, 0),
+        (@IdUser, 'Achievement', 'WorkoutCount', 100, 100, 0),
 
-           (@IdUser, 'Achievement', 'WorkoutTime', 1, 10, 0),
-           (@IdUser, 'Achievement', 'WorkoutTime', 50, 10, 0),
-           (@IdUser, 'Achievement', 'WorkoutTime', 100, 25, 0),
-           (@IdUser, 'Achievement', 'WorkoutTime', 150, 30, 0),
-           (@IdUser, 'Achievement', 'WorkoutTime', 200, 50, 0),
+        (@IdUser, 'Achievement', 'WorkoutTime', 1, 10, 0),
+        (@IdUser, 'Achievement', 'WorkoutTime', 50, 10, 0),
+        (@IdUser, 'Achievement', 'WorkoutTime', 100, 25, 0),
+        (@IdUser, 'Achievement', 'WorkoutTime', 150, 30, 0),
+        (@IdUser, 'Achievement', 'WorkoutTime', 200, 50, 0),
 
-           (@IdUser, 'Achievement', 'VolumeWorkout', 100, 5, 0),
-           (@IdUser, 'Achievement', 'VolumeWorkout', 500, 10, 0),
-           (@IdUser, 'Achievement', 'VolumeWorkout', 1000, 30, 0),
-           (@IdUser, 'Achievement', 'VolumeWorkout', 1500, 50, 0),
-           (@IdUser, 'Achievement', 'VolumeWorkout', 2000, 100, 0),
+        (@IdUser, 'Achievement', 'VolumeWorkout', 100, 5, 0),
+        (@IdUser, 'Achievement', 'VolumeWorkout', 500, 10, 0),
+        (@IdUser, 'Achievement', 'VolumeWorkout', 1000, 30, 0),
+        (@IdUser, 'Achievement', 'VolumeWorkout', 1500, 50, 0),
+        (@IdUser, 'Achievement', 'VolumeWorkout', 2000, 100, 0),
 
-           (@IdUser, 'Achievement', 'VolumeTotal', 1000, 5, 0),
-           (@IdUser, 'Achievement', 'VolumeTotal', 10000, 10, 0),
-           (@IdUser, 'Achievement', 'VolumeTotal', 20000, 30, 0),
-           (@IdUser, 'Achievement', 'VolumeTotal', 30000, 50, 0),
-           (@IdUser, 'Achievement', 'VolumeTotal', 100000, 100, 0),
+        (@IdUser, 'Achievement', 'VolumeTotal', 1000, 5, 0),
+        (@IdUser, 'Achievement', 'VolumeTotal', 10000, 10, 0),
+        (@IdUser, 'Achievement', 'VolumeTotal', 20000, 30, 0),
+        (@IdUser, 'Achievement', 'VolumeTotal', 30000, 50, 0),
+        (@IdUser, 'Achievement', 'VolumeTotal', 100000, 100, 0),
 
-           (@IdUser, 'Achievement', 'Level', 3, 5, 0),
-           (@IdUser, 'Achievement', 'Level', 10, 10, 0),
-           (@IdUser, 'Achievement', 'Level', 50, 30, 0),
-           (@IdUser, 'Achievement', 'Level', 70, 50, 0)
+        (@IdUser, 'Achievement', 'Level', 3, 5, 0),
+        (@IdUser, 'Achievement', 'Level', 10, 10, 0),
+        (@IdUser, 'Achievement', 'Level', 50, 30, 0),
+        (@IdUser, 'Achievement', 'Level', 70, 50, 0)
+
+	INSERT INTO [dbo].[Image]
+		([IdUser], [Path], [Cost], [Enable])
+	VALUES
+		(@IdUser, '0', 0, 1),
+		(@IdUser, '1', 10, 0),
+		(@IdUser, '2', 10, 0),
+		(@IdUser, '3', 10, 0)
 END
